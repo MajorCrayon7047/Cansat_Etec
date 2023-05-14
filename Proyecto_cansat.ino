8667af70f8e75499449401a278d74e143ee9a6c7
MajorCrayon7047 <110138925+MajorCrayon7047@users.noreply.github.com>
2023-05-13 21:35:52 -0300

    Se agrego la comunicacion LoRa

--- Proyecto_cansat.ino
+++ Proyecto_cansat.ino
@@ -5,6 +5,7 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include <BH1750.h>
+#include "heltec.h"
 
 //variables globales
 float bmp180[3];
@@ -14,9 +15,11 @@ uint16_t luz;
 float gasVoltage;
 #define DHTPIN 2
 #define DHTTYPE    DHT22
+#define BAND 915E6  //you can set band here directly,e.g. 433E6,868E6,915E6
 const int mpuAddress = 0x68;  //Puede ser 0x68 o 0x69
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
+unsigned long ultima=0;
 
 //objetos:
 Adafruit_BMP085 bmp;
@@ -48,7 +51,7 @@ void read_light(){
     luz = Luxometro.readLightLevel();
 }
 void read_gas(){
-    int read = analogRead(A0)
+    int read = analogRead(A0);
     gasVoltage = read * (3.3 / 1023.0);
 }
 
@@ -63,8 +66,47 @@ void setup(){
     mpu.initialize();
     Luxometro.begin(BH1750_CONTINUOUS_HIGH_RES_MODE);
     Serial.println(mpu.testConnection() ? F("IMU iniciado correctamente") : F("Error al iniciar IMU"));
+    Heltec.begin(true /*DisplayEnable Enable*/, true /*Heltec.LoRa Disable*/, true /*Serial Enable*/, true /*PABOOST Enable*/, BAND /*long BAND*/);  
 }
 
 void loop(){
-	//ACA VA LA COMUNICACION DE LORA POINT TO POINT (PTP)
+    if (millis()>=ultima){
+    ultima = millis() + 500;
+	// leer sensores
+    read_bmp();
+    read_dht();
+    read_gas();
+    read_light();
+    read_MPU();
+
+    //Envio de paquete via LoRa en formato JSON para decodificarlo en el receptor
+    LoRa.beginPacket();
+    LoRa.setTxPower(5,RF_PACONFIG_PASELECT_PABOOST);
+    LoRa.print(F("{hum:"));
+    LoRa.print(dht11[1]);
+    LoRa.print(",temp:");
+    LoRa.print(bmp180[0]);
+    LoRa.print(",press:");
+    LoRa.print(bmp180[1]);
+    LoRa.print(",alt:");
+    LoRa.print(bmp180[2]);
+    LoRa.print(",gas:");
+    LoRa.print(gasVoltage);
+    LoRa.print(",luz:");
+    LoRa.print(luz);
+    LoRa.print(",acele:[");
+    LoRa.print(mpu6050[0]);
+    LoRa.print(",");
+    LoRa.print(mpu6050[1]);
+    LoRa.print(",");
+    LoRa.print(mpu6050[2]);
+    LoRa.print("],giro:");
+    LoRa.print(mpu6050[3]);
+    LoRa.print(",");
+    LoRa.print(mpu6050[4]);
+    LoRa.print(",");
+    LoRa.print(mpu6050[5]);
+    LoRa.print("}");
+    LoRa.endPacket();
+    }
 }
